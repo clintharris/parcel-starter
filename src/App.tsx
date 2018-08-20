a50f52636d8f81fac58f816036fe6fc3d0c04d1e
Clint Harris <github@kxpost.net>
2018-08-20 16:17:29 -0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from '@src/App.scss';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className={styles.root}><h5>Hello, world!</h5></div>
+    );
+  }
+}
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'modern-normalize/modern-normalize.css';
+
+import App from '@src/App';
+
+console.info('Starting up...');
+
+ReactDOM.render(<App/>, document.getElementById('root'));
